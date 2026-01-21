2d3c1ddc924bfad9d2a2971be482aff4419753f0
EriSalcedo <monis2602@gmail.com>
2026-01-21 14:47:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const sello = document.getElementById("sello");
 const musica = document.getElementById("musica");
+const envelope = document.querySelector(".envelope");
+const invitacion = document.getElementById("invitacion");
 
 sello.addEventListener("click", () => {
   musica.volume = 0.7;
   musica.play();
+
+  envelope.classList.add("abierto");
+
+  setTimeout(() => {
+    invitacion.classList.add("visible");
+  }, 800);
 });
